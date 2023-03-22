4be0cf253eced85c34cf46474ce2cc3fa16dffaa
Emdee82 <mikedonnelly82@gmail.com>
2023-03-22 16:55:01 +0000

    - Added state-specific processing to AI prompt - Cleaned up logging.

--- commands/ai/chat-gpt.js
+++ commands/ai/chat-gpt.js
@@ -9,20 +9,58 @@ const configuration = new OpenAI.Configuration({
 const openai = new OpenAI.OpenAIApi(configuration);
 
 const path = "commands/ai/prompt.txt";
-const samplePath = "commands/ai/sample-response3.txt";
 
 var prompt = "";
 
-const loadPrompt = () => {
+const calculateScore = (state, playerId) => {  
+  const players = state.players;
+  if(!players[playerId]) {
+    return 0;
+  }
+
+  // Calculate total score
+  let score = players[playerId].basePoints;
+
+  if (players[playerId].bonuses) {
+    let bonusKeys = Object.keys(players[playerId].bonuses);
+    bonusKeys.forEach(bonusKey => {
+      let bonus = state.bonuses[bonusKey];
+      score = score + (bonus.points * players[playerId].bonuses[bonusKey].times);
+    });
+
+    return score;
+  };
+}
+
+const insertPicks = (state, playerId) => {
+  var picks = "";
+  state.players[playerId].picks.forEach(pick => {
+    picks = picks + "\r\n    " + (state.celebs[pick].isAlive ? state.celebs[pick].name : `${state.celebs[pick].name} (Deceased)`);
+  });
+
+  return picks;
+}
+
+const insertGameState = (state) => {
+  var stateReplace = "";
+  state.playerKeys.forEach(playerId => {
+    stateReplace = stateReplace + "\r\n"
+    + "- Name: " + state.players[playerId].name + "\r\n"
+    + "  Gender: Male\r\n" // TODO: Update this to be more inclusive.
+    + "  Points: " + calculateScore(state, playerId) + "\r\n"
+    + "  Picks: " + insertPicks(state, playerId);
+  });
+
+  return stateReplace;
+}
+
+const loadPrompt = (state) => {
     if (fs.existsSync(path)) {
         prompt = fs.readFileSync(path, "utf8");
     }
-}
 
-const sampleResponse = () => {
-    if (fs.existsSync(path)) {
-        return fs.readFileSync(samplePath, "utf8");
-    }
+    var gameStateInsert = insertGameState(state);
+    prompt = prompt.replace(/\[STATEREPLACE\]/ig, gameStateInsert);
 }
 
 exports.chatGpt = async (stateFuncs, msg) => {
@@ -34,11 +72,11 @@ exports.chatGpt = async (stateFuncs, msg) => {
     }
 
     try {
-      loadPrompt();
       const state = stateFuncs.getState();
+      loadPrompt(state);
+
       var messageContent = msg.content.replace(`<@${BOT_USER_ID}>`, 'DeathBot,');
       console.log(new Date(), `[chat-gpt]: ${msg.author.username} has asked:`, messageContent);
-
       var sender = state.playerKeys.filter(x => (msg.author.username +'#' + msg.author.discriminator) == state.players[x].userId);
       
       const completion = await openai.createChatCompletion({
@@ -50,26 +88,20 @@ exports.chatGpt = async (stateFuncs, msg) => {
         max_tokens: 1500
       });
       
-      // console.log(new Date(), "[chat-gpt]: Completion: ", completion);
-      // console.log(new Date(), "[chat-gpt]: Choices: ", completion.data.choices);
-      // console.log(new Date(), "[chat-gpt]: Choices[0]: ", completion.data.choices[0]);
-      // console.log(new Date(), "[chat-gpt]: Choices[0].text: ", completion.data.choices[0].message);
-      
       var responses = completion.data.choices[0].message.content.split(/\r?\n\r?\n/);
       responses.forEach(res => {
         if (res && res.match(/[a-zA-Z0-9]+/)) {
-          console.log(new Date(), "[chat-gpt]: Replying with: ", res);
           msg.channel.send(res.replace(/deathbot/ig, '**DeathBot**'));
         }
-      })      
+      })
     } 
     catch(error) {
       if (error.response) {
         console.error(error.response.status, error.response.data);
-        msg.reply("An error occurred with this command, causing it to fail.");
+        msg.reply(new Date(), "[chat-gpt]: An error occurred with this command, causing it to fail.");
       } else {
-        console.error(`Error with OpenAI API request: ${error.message}`);
-        msg.reply(`Error with OpenAI API request: ${error.message}`);
+        console.error(new Date(), `[chat-gpt]: Error with OpenAI API request: ${error.message}`);
+        msg.reply(new Date(), `[chat-gpt]: Error with OpenAI API request: ${error.message}`);
       }
     }
   } else {
